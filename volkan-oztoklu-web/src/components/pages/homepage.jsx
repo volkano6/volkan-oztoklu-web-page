ddde2c9fce50ea7077207df7ff23a49ff461267d
Volkan Öztoklu <volkanoztoklu@gmail.com>
2023-03-02 23:43:39 +0300

    draft is created

--- volkan-oztoklu-web/src/components/pages/homepage.jsx
+++ volkan-oztoklu-web/src/components/pages/homepage.jsx
@@ -1,10 +1,11 @@
 import Navbar from '../layout/navbar'
-
+import About from '../homepage/about';
 function Homepage() {
   return (
     <div>
-      <div class="min-h-screen min-w-full bg-cover bg-center bg-no-repeat bg-[url('../../background-2.jpg')]"> 
+      <div class="text-neutral-300 min-h-screen min-w-full bg-cover bg-center bg-no-repeat bg-[url('../src/assets/images/header-background.webp')]"> 
           <Navbar />
+          <About />
            
       </div>
       <div>
